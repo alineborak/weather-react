6a2e2f8df8e66eb4b40f5ca27b7cc00305c8807b
Aline <alinebora@gmail.com>
2022-06-07 21:23:18 +0200

    Changing colors

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -25,7 +25,7 @@ export default function WeatherIcon(props) {
     return (
         <ReactAnimatedWeather
             icon={codeMapping[props.code]}
-            color="#70757A"
+            color="#e1e9f1"
             size={78}
             animate={true}
         />
--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Weather from "./Weather";
+import bg from "./sky.jpg";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
       <Weather defaultCity="Amsterdam" />
       <div>
         <small>
-          <a href="https://github.com/alineborak/weather-react" target="blank" rel="noreferrer" >Open-source code </a> by Aline Bora-Kloppert
+          <a
+            href="https://github.com/alineborak/weather-react"
+            target="blank"
+            rel="noreferrer"
+          >
+            Open-source code{" "}
+          </a>{" "}
+          by Aline Bora-Kloppert
         </small>
       </div>
     </div>
   );
 }
-<img src="./sky.png" alt="sky" class="sky" />
+<img src="./sky.png" alt="sky" class="sky" />;
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
