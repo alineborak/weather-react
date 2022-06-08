eb6a1cfe33ba37bd0cb1cb4ba7fa6d02247bd63a
Aline <alinebora@gmail.com>
2022-06-08 20:38:09 +0200

    Adding linkedin url

--- src/index.js
+++ src/index.js
@@ -10,14 +10,21 @@ function App() {
       <Weather defaultCity="Amsterdam" />
       <div>
         <small>
+          Open-source{" "}
           <a
             href="https://github.com/alineborak/weather-react"
             target="blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
-            Open-source code{" "}
-          </a>{" "}
-          by Aline Bora-Kloppert
+            code on Github{" "}
+          </a>
+          by <a
+            href="https://www.linkedin.com/in/aline-bora-kloppert/"
+            target="blank"
+            rel="noopener noreferrer"
+          >
+            Aline Bora-Kloppert
+          </a>
         </small>
       </div>
     </div>
