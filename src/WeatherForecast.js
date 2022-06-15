32c21ea898d4a8ec8a3a1c5de96c2e70cf655a6c
Aline <alinebora@gmail.com>
2022-06-15 15:34:17 +0200

    Adding missing array return to function

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -28,7 +28,7 @@ export default function WeatherForecast(props) {
                                     <WeatherForecastDay data={dailyForecast} />
                                 </div>
                             );
-                        }
+                        } else { return null; }
                     })}
                 </div>
             </div>
